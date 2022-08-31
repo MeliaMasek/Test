c9580b018623e51b41fa5b1bce8714f47b99c5c7
Melia Masek <Masekmelia@gmail.com>
2022-08-30 23:39:54 -0600

    scriptable objects creation

---
+++ Testgame/Assets/Scripts/FloatData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+
+//All code is for testing purposes and is sourced from Anthony Romrell
+public class FloatData : ScriptableObject
+{
+    public float value;
+}
